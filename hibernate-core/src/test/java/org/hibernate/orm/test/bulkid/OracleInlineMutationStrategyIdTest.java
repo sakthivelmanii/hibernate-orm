89aeb7250949206c683cc7ce391c418d101c3849
Sakthivel Subramanian <sakthipvmj@gmail.com>
2025-10-18 18:08:37 +0530

    Skip oracle test

--- hibernate-core/src/test/java/org/hibernate/orm/test/bulkid/OracleInlineMutationStrategyIdTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bulkid/OracleInlineMutationStrategyIdTest.java
@@ -4,12 +4,16 @@
  */
 package org.hibernate.orm.test.bulkid;
 
+import org.hibernate.dialect.SpannerPostgreSQLDialect;
+import org.hibernate.testing.orm.junit.SkipForDialect;
+
 /**
  * Special test that tries to update 1100 rows. Oracle only supports up to 1000 parameters per in-predicate,
  * so we want to test if this scenario works.
  *
  * @author Vlad Mihalcea
  */
+@SkipForDialect( dialectClass = SpannerPostgreSQLDialect.class)
 public class OracleInlineMutationStrategyIdTest extends InlineMutationStrategyIdTest {
 
 	@Override
